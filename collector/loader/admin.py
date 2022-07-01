1b07d80c94ba5c7c059ab63e611b9452d0a5b07a
usman <tasuevusman@gmail.com>
2022-07-01 13:04:24 +0300

    file loader and admin panel

---
+++ collector/loader/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from loader.models import Device, DataFile
+
+
+admin.site.register(Device)
+admin.site.register(DataFile)
---
+++ collector/loader/models.py
@@ -0,0 +1,26 @@
+from pydoc import describe
+from django.db import models
+from django.utils import timezone
+import datetime
+
+class Device(models.Model):
+    name = models.CharField(max_length=500)
+    description = models.CharField(max_length=2000)
+    location = models.CharField(max_length=2000, default='-1')
+
+    def __str__(self):
+        return f"Device: {self.name}, location: {self.location}, description: {self.description}"
+
+
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return 'device_{0}_{1}/{2}/{3}'.format(instance.device.id, instance.device.name, datetime.date.today().isoformat(), filename)
+
+
+class DataFile(models.Model):
+    device = models.ForeignKey(Device, on_delete=models.CASCADE)
+    file = models.FileField(upload_to=user_directory_path)
+    date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"Device: {self.device.id} {self.device.name}, date: {self.date} file: {self.file}"
